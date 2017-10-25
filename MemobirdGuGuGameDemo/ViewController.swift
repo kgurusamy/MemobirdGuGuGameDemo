1e2ea8656fdeb529ac4be099c83736bc94da822b
Kumaravel G <kgurusamy@cricut.com>
2017-10-25 14:44:22 +0530

    Initial Commit files

---
+++ MemobirdGuGuGameDemo/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  MemobirdGuGuGameDemo
+//
+//  Created by Kumaravel on 24/10/17.
+//  Copyright © 2017 Oottru. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let scene = GameScene(size: view.bounds.size)
+        let skView = view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .resizeFill
+        skView.presentScene(scene)
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
