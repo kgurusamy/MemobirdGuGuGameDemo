9f5a4f6d2f67e5f8bb96bb9a1ad70000813ce942
Kumaravel G <kgurusamy@cricut.com>
2017-10-31 12:06:56 +0530

    After the shot ,Displaying corresponding response
    After the shot ,Displaying corresponding response

--- MemobirdGuGuGameDemo/GameScene.swift
+++ MemobirdGuGuGameDemo/GameScene.swift
@@ -52,7 +52,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
   // 1
   let player = SKSpriteNode(imageNamed: "player")
   var monstersDestroyed = 0
-
+  let myLabel = SKLabelNode(fontNamed:"Chalkduster")
+ 
   override func didMove(to view: SKView) {
     // 2
     backgroundColor = SKColor.white
@@ -61,16 +62,25 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     // 4
     addChild(player)
     
+    myLabel.text = ""
+    myLabel.fontColor = SKColor.black
+    myLabel.fontSize = 20
+    myLabel.position = CGPoint(x:(self.view?.center.x)!, y:(self.view?.center.y)!+300)
+    self.addChild(myLabel)
+    
     physicsWorld.gravity = CGVector.zero
     physicsWorld.contactDelegate = self
     
     run(SKAction.repeatForever(
       SKAction.sequence([
+        
         SKAction.run(addMonster),
+        SKAction.wait(forDuration: 0.5), // Speed of the monster
+        SKAction.run(addMonster_black),
         SKAction.wait(forDuration: 0.5) // Speed of the monster
       ])
     ))
-    
+
     let backgroundMusic = SKAudioNode(fileNamed: "background-music-aac.caf")
     backgroundMusic.autoplayLooped = true
     addChild(backgroundMusic)
@@ -89,7 +99,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
    
     // Create sprite
     let monster = SKSpriteNode(imageNamed: "monster")
-   
+    monster.name = "bluebird"
     monster.physicsBody = SKPhysicsBody(rectangleOf: monster.size) // 1
     monster.physicsBody?.isDynamic = true // 2
     monster.physicsBody?.categoryBitMask = PhysicsCategory.Monster // 3
@@ -121,6 +131,43 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     monster.run(SKAction.sequence([actionMove, /*loseAction, */actionMoveDone]))
    
   }
+    
+    func addMonster_black() {
+        
+        // Create sprite
+        let monster = SKSpriteNode(imageNamed: "monster_black")
+        monster.name = "blackbird"
+        monster.physicsBody = SKPhysicsBody(rectangleOf: monster.size) // 1
+        monster.physicsBody?.isDynamic = true // 2
+        monster.physicsBody?.categoryBitMask = PhysicsCategory.Monster // 3
+        monster.physicsBody?.contactTestBitMask = PhysicsCategory.Projectile // 4
+        monster.physicsBody?.collisionBitMask = PhysicsCategory.None // 5
+        
+        // Determine where to spawn the monster along the Y axis
+        let actualY = random(min: (size.height*0.5), max: (size.height-monster.size.height*2))
+        
+        // Position the monster slightly off-screen along the right edge,
+        // and along a random position along the Y axis as calculated above
+        monster.position = CGPoint(x: size.width + monster.size.width/2, y: actualY)
+        
+        // Add the monster to the scene
+        addChild(monster)
+        
+        // Determine speed of the monster
+        let actualDuration = random(min: CGFloat(2.0), max: CGFloat(5.0))
+        
+        // Create the actions
+        let actionMove = SKAction.move(to: CGPoint(x: -monster.size.width/2, y: actualY), duration: TimeInterval(actualDuration))
+        let actionMoveDone = SKAction.removeFromParent()
+        
+        /*let loseAction = SKAction.run() {
+         let reveal = SKTransition.flipHorizontal(withDuration: 0.5)
+         let gameOverScene = GameOverScene(size: self.size, won: false)
+         self.view?.presentScene(gameOverScene, transition: reveal)
+         }*/
+        monster.run(SKAction.sequence([actionMove, /*loseAction, */actionMoveDone]))
+        
+    }
   
   override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
   
@@ -135,7 +182,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     // 2 - Set up initial location of projectile
     let projectile = SKSpriteNode(imageNamed: "projectile")
     projectile.position = player.position
-   
+    projectile.name = "projectile"
     projectile.physicsBody = SKPhysicsBody(circleOfRadius: projectile.size.width/2)
     projectile.physicsBody?.isDynamic = true
     projectile.physicsBody?.categoryBitMask = PhysicsCategory.Projectile
@@ -163,18 +210,34 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
    
     // 9 - Create the actions
     let actionMove = SKAction.move(to: realDest, duration: 2.0)
+   
     let actionMoveDone = SKAction.removeFromParent()
     projectile.run(SKAction.sequence([actionMove, actionMoveDone]))
-   
+
   }
   
   func projectileDidCollideWithMonster(projectile: SKSpriteNode, monster: SKSpriteNode) {
-    print("Hit")
+    //print("Hit")
+    if(monster.name == "bluebird")
+    {
+        myLabel.alpha = 1.0
+        myLabel.text = "You hit bluebird"
+        myLabel.run(SKAction.fadeOut(withDuration: 1.5))
+        print("You hit bluebird")
+    }
+    else if(monster.name == "blackbird")
+    {
+        myLabel.alpha = 1.0
+        myLabel.text = "you hit blackbird"
+        myLabel.run(SKAction.fadeOut(withDuration: 1.5))
+        print("you hit blackbird")
+    }
+    
     projectile.removeFromParent()
     monster.removeFromParent()
     
     monstersDestroyed += 1
-    if (monstersDestroyed > 10) {
+    if (monstersDestroyed > 30) {
       let reveal = SKTransition.flipHorizontal(withDuration: 0.5)
       let gameOverScene = GameOverScene(size: self.size, won: true)
       self.view?.presentScene(gameOverScene, transition: reveal)
@@ -201,8 +264,48 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         projectile = secondBody.node as? SKSpriteNode {
         projectileDidCollideWithMonster(projectile: projectile, monster: monster)
       }
+      
     }
-   
   }
+    
+    override func update(_ currentTime: TimeInterval) {
+        super.update(currentTime)
+        
+        // ... every other update logic
+       
+       /* for node in nodesToCheck {
+            if node.position.y < -node.size.height/2.0 {
+                node.removeFromParent()
+                gameOver()
+            }
+        }*/
+        
+        // Loop over all nodes in the scene
+        self.enumerateChildNodes(withName: "projectile") {
+            node, stop in
+            if (node is SKSpriteNode) {
+                
+                let projectile = node as! SKSpriteNode
+                if(projectile.name == "projectile")
+                {
+                    if(projectile.position.x < 0 || projectile.position.x > self.size.width || projectile.position.y > self.size.height){
+                        self.myLabel.alpha = 1.0
+                        self.myLabel.text = "target missed!"
+                        self.myLabel.run(SKAction.fadeOut(withDuration: 1.5))
+                        print("target missed!")
+                    }
+                 
+                    //print("Y Position : \(projectile.position.y)")
+                }
+                // Check if the node is not in the scene
+                /*if (sprite.position.x < -sprite.size.width/2.0 || sprite.position.x > self.size.width+sprite.size.width/2.0
+                    || sprite.position.y < -sprite.size.height/2.0 || sprite.position.y > self.size.height+sprite.size.height/2.0) {
+                    sprite.removeFromParent()
+                }*/
+            }
+        }
+    }
 
 }
+
+
--- MemobirdGuGuGameDemo/ViewController.swift
+++ MemobirdGuGuGameDemo/ViewController.swift
@@ -16,8 +16,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         let scene = GameScene(size: view.bounds.size)
         let skView = view as! SKView
-        skView.showsFPS = true
-        skView.showsNodeCount = true
+        //skView.showsFPS = true
+        //skView.showsNodeCount = true
         skView.ignoresSiblingOrder = true
         scene.scaleMode = .resizeFill
         skView.presentScene(scene)
